7f8810d0c15e272f94ac8bdf4876d4110c95be75
AdamBabinicz <puaro@vp.pl>
2018-05-15 19:38:11 +0200

    wersja-lapek

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-$(document).ready(function(){
-    init();
-})
-function init(){
-    var toolTimeline = new TimelineLite({paused:true});
-    var duration = .5;
-    toolTimeline.add(TweenLite.to('#toolBox', duration, {y:-100, ease:Linear.easeInOut}));
-    toolTimeline.add(TweenLite.to('#weightLifer', duration, {y:-100, ease:Bounce.easeInOut}));
-    toolTimeline.add(TweenLite.to('#crazy', duration, {y:-100, ease:Elastic.easeInOut}));
-    toolTimeline.add(TweenLite.to('#leaf', duration, {y:-100, ease:Back.easeInOut}));
-    
-    $("#start").click(function(){
-       toolTimeline.play(); 
-    });
-    $("#pause").click(function(){
-       toolTimeline.pause(); 
-    });
-    $("#stop").click(function(){
-       toolTimeline.stop(); 
-    });
-    $("#reverse").click(function(){
-       toolTimeline.reverse(); 
-    });
-   
+$(window).load(function () {
+	init();
+});
+
+function init() {
+	var toolTimeline = new TimelineMax();
+	var duration = .5;
+	toolTimeline.from('.mainTitle', duration, {
+		opacity: 0,
+		scale: 25,
+		ease: Linear.easeInOut
+	});
+	/*	toolTimeline.to('#weightLifer', duration, {
+			y: -100,
+			ease: Bounce.easeInOut
+		});
+		toolTimeline.to('#crazy', duration, {
+			y: -100,
+			ease: Elastic.easeInOut
+		});
+		toolTimeline.to('#leaf', duration, {
+			y: -100,
+			ease: Back.easeInOut
+		}); */
+
+
 }
